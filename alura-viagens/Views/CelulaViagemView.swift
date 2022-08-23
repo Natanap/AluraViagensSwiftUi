7148f35f7fbbcacc326d95912351eddaf6eb3b4f
Natanael Alves <natanalvespr@gmail.com>
2022-08-23 14:30:03 -0300

    Ajustando a celula

--- alura-viagens/Views/CelulaViagemView.swift
+++ alura-viagens/Views/CelulaViagemView.swift
@@ -10,18 +10,25 @@ import SwiftUI
 struct CelulaViagemView: View {
     
     var viagem:Viagem
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
     var body: some View {
         
         VStack (alignment: .leading){
             Text(viagem.titulo)
-            Image(viagem.imagem).resizable()
-                .frame( height: 125)
+                .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14: 24))
+            Image(viagem.imagem)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame( height: self.horizontalSizeClass == .compact ? 125: 200)
+                .clipped()
             
             HStack {
                 Text(viagem.quantidadeDeDias)
+                    .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14: 24))
                 Spacer()
                 Text(viagem.valor)
+                    .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14: 24))
             }
         }
     }
